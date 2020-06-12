f1457c26e82434147a584681ec00b66670c35161
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-06-12 13:03:57 +0300

    Blank app. Stable.

---
+++ App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by 土井愛己 on 2017/11/19.
+//  Copyright © 2017年 aiki.doi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
